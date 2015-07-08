2ae8463ff6b6b76c4ed8f03ef6adcb4c22953db8
edgarw11 <edgarw11@gmail.com>
2015-07-07 22:48:52 -0300

    Add column senha for clientes. Rename column data to data_compra

--- sql/insert_templates.sql
+++ sql/insert_templates.sql
@@ -1,10 +1,10 @@
-INSERT INTO `webshop`.`clientes` (`id`, `nome`, `cpf`, `telefone`) 
-VALUES (NULL, NULL, NULL, NULL);
+INSERT INTO `webshop`.`clientes` (`id`, `nome`, `cpf`, `telefone`, `senha`) 
+VALUES (NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `webshop`.`enderecos` (`id`, `desc`, `rua`, `numero`, `bairro`, `cidade`, `cep`, `clientes_id`) 
 VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
-INSERT INTO `webshop`.`pedidos` (`id`, `data`, `data_mod`, `status`, `valor_produtos`, `valor_frete`, `descontos`, `clientes_id`) 
+INSERT INTO `webshop`.`pedidos` (`id`, `data_compra`, `data_mod`, `status`, `valor_produtos`, `valor_frete`, `descontos`, `clientes_id`) 
 VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `webshop`.`produtos` (`id`, `nome`, `desc`, `valor`) 
